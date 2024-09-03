14dc50be50cac1816156c5522c01640623a72307
RAHUL-107 <167115654+RAHUL-107@users.noreply.github.com>
2024-09-03 10:26:37 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)// we are using nodejs, not browser
+
+//data types
+//number
+//bigint= to be used when there is a long number
+//string=""
+//boolean= true or false
+//null= stand alone value
+//undefined= when there is no value given 
+//symbol= unique
+
+
+//object
+
+console.log(typeof null);
---
+++ basics/variable.js
@@ -0,0 +1,14 @@
+const account_Id = 14453
+let accountemail= "hitesh@gmail.com"
+var accountpassword="233"
+accountcity="jaipur"
+
+//account_Id = 234(not allowed)
+accountemail="skibom@gmail.com"
+accountpassword="323232"
+accountcity="bengaluru"
+/*
+not ever to use var because of the problem of blockscope  and functional scope  
+*/
+
+console.table([accountemail,accountpassword,accountcity])
