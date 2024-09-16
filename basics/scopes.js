7fb8da226c737a8e78abd201b3ffd760397514f1
RAHUL-107 <167115654+RAHUL-107@users.noreply.github.com>
2024-09-16 15:10:56 +0000

    scope

---
+++ basics/scopes.js
@@ -0,0 +1,29 @@
+// function one(){
+
+//     const username = "rahul"
+
+//     function two(){
+//         const website = "youtue"
+//         console.log(username);
+//     }
+//     console.log(website);
+//     two()
+// }
+
+// one()
+
+// console.log(addtwo(2))//here function can be accessed befor declaration
+
+// function addtwo(num){
+//     return num +2
+
+// }
+
+
+// console.log(addtwo(2))//here it will give an error as function is declared later
+// const addtwo = function(num){
+//     return num +2
+// }
+
+
+
