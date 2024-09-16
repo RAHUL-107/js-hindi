326fab1c164f2c8209f9793cc1dec5470c69ed20
RAHUL-107 <167115654+RAHUL-107@users.noreply.github.com>
2024-09-16 15:36:10 +0000

    arrow

---
+++ basics/arrowfunction.js
@@ -0,0 +1,37 @@
+// const user= {
+//     username:"hitesh ",
+//     price:333,
+
+//     welcomemessage: function(){
+//         console.log(`${this.username}, welcome to website`);
+//         console.log(this)
+//     }
+
+// }
+
+// user.welcomemessage()
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(username)
+// }
+
+// chai()
+
+// const chai = () =>{
+//     let username = "hitesh"
+//     console.log(this.username);
+
+// }
+// chai()
+
+// const addtwo = (num1,num2) => {
+//     return num1+num2
+// }
+
+// console.log(addtwo(3,4))
+
+const addtwo = (num1,num2) => (num1+num2)//implicit retrun= where we dont have to use return keyword and curly braces instead we use ()
+console.log(addtwo(3,4))
+
+
