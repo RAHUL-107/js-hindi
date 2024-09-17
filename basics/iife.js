b24b4f502e71b600cb95f77db31bf92d7cb9805a
RAHUL-107 <167115654+RAHUL-107@users.noreply.github.com>
2024-09-17 13:08:48 +0000

    iife

---
+++ basics/iife.js
@@ -0,0 +1,13 @@
+//global mai  polution ko hatane ke liye jo use hota hai use iife kehte hai
+(function chai(){
+    console.log(`db connected`)
+})
+();
+
+( ()=>{
+    console.log(`db two connected`)
+})
+()
+
+ 
+
